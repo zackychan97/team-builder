d3fda7d73f57dd3f717613822e9e54f5b2df1eff
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-16 15:31:15 -0500

    setup my state and gave default value and am now in form js building out my markup

---
+++ team-builder/src/Components/Form.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const daForm = props => {
+    const [employeeInfo, setEmpInfo] = useState({
+        employeeName: '',
+        employeeJob: '',
+        employeeEmail: ''
+    });
+
+  
+}
+
+export default daForm;
